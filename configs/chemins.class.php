bcbfd9953c3e1adb501a30c3da5d58b95edb7663
Loukade <65962183+Loukade@users.noreply.github.com>
2021-06-14 16:28:11 +0200

    Add files via upload

---
+++ configs/chemins.class.php
@@ -0,0 +1,29 @@
+<?php
+class Chemins {
+    //Chemins à l'intérieur du dossier APPLICATION
+    const MODELES ="application/modeles/"; 
+    
+    const VUES ="application/vues/";
+    const VUES_PERMANENTES ="application/vues/permanentes/";
+    const VUES_ADMIN ="application/vues/partie_admin/";
+    
+    const CONTROLEURS ="application/controleurs/";
+    
+    //Chemins à l'intérieur du dossier PUBLIC
+    const IMAGES ="public/images/";
+    const IMAGES_PRODUIT ="public/images/produits/";
+    const JS ="public/js/";
+    const STYLES ="public/styles/";
+    
+    //Autres chemin du site
+    const CONFIGS ="configs/";
+    const LIBS ="Libs/";
+    const AJAX = "Libs/AJAX/";
+}
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
